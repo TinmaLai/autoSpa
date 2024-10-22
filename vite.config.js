ff09661c2e5755e7bda54f94908e76a885125b66
Tín Nguyễn <54711104+TinmaLai@users.noreply.github.com>
2024-10-22 21:45:56 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'dist', // Thư mục output
+    rollupOptions: {
+      // Bạn có thể thêm các cấu hình khác ở đây nếu cần
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
